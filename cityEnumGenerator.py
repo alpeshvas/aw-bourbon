0b9f10b5ddb4f48da1bd1d838f0bc87bb0b31607
Vas Alpesh <alpeshvash@gmail.com>
2017-10-03 17:27:07 +0530

    Format fix

--- cityEnumGenerator.py
+++ cityEnumGenerator.py
@@ -19,13 +19,14 @@ def generateEnum():
 	for city in cityReader:
 		if city['cityCode'] not in curCities:
 			GenerateEnum.write( "\n" +tab+city['cityCode']+"(")
-			GenerateEnum.write( "\n" +tab2+boundary+city['cityName']+boundary)
-			GenerateEnum.write( "\n" +tab2+"Country."+city['countryCode'])
-			GenerateEnum.write( "\n" +tab2+boundary+city['timezone']+boundary)
-			GenerateEnum.write( "\n" +tab2+city['cityLat']+"d")
-			GenerateEnum.write( "\n" +tab2+city['cityLong']+"d")
-			GenerateEnum.write( "\n" +tab2+'""')
-			GenerateEnum.write( "\n" +tab2+'""')
-			GenerateEnum.write( "\n" +tab2+'""')
+			GenerateEnum.write( "\n" +tab2+boundary+city['cityName']+boundary+",")
+			GenerateEnum.write( "\n" +tab2+"Country."+city['countryCode']+",")
+			GenerateEnum.write( "\n" +tab2+boundary+city['timezone']+boundary+",")
+			GenerateEnum.write( "\n" +tab2+city['cityLat']+"d"+",")
+			GenerateEnum.write( "\n" +tab2+city['cityLong']+"d"+",")
+			GenerateEnum.write( "\n" +tab2+'""'+",")
+			GenerateEnum.write( "\n" +tab2+'""'+",")
+			GenerateEnum.write( "\n" +tab2+'""'+",")
+			GenerateEnum.write( "\n" +tab+city['cityCode']+"),")
 
 generateEnum()
